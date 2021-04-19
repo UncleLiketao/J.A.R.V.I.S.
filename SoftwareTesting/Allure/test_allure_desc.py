462a08082c0641cc85decfe4b01ec4826f5432ec
jeremy.li <414480664@qq.com>
2021-04-19 22:10:20 +0800

    [add] 添加pytest和allure示例文件

---
+++ SoftwareTesting/Allure/test_allure_desc.py
@@ -0,0 +1,15 @@
+﻿import allure
+import pytest
+
+@allure.feature('test_module_01')
+@allure.story('test_story_01')
+#test_case_01为用例title
+def test_case_01():
+    """
+    用例描述：这是用例描述，Test case 01，描述本人
+    """
+    #注释为用例描述
+    assert 0
+
+if __name__ == '__main__':
+    pytest.main(['-s', '-q', '--alluredir', './report/xml'])
---
+++ SoftwareTesting/Pytest/test_xdist.py
@@ -0,0 +1,6 @@
+﻿import pytest
+import time
+
+@pytest.mark.parametrize('x',list(range(10)))
+def test_somethins(x):
+    time.sleep(1)
